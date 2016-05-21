2c1ac4f6cac1484257cd1a75756f72a4369b04b2
Bohnna Chhim <info@bohnna.com>
2016-05-20 21:08:51 -0400

    Separated header into head.php and header.php - head.php contains the actual <head></head> information - header.php is for layout

--- templates/header.php
+++ templates/header.php
@@ -1,55 +1,19 @@
-<?php
-/**
- * The template for displaying the header
- *
- * @package Smores
- * @since Smores 2.0
- */
+<?php 
+get_template_part('templates/head');
 ?>
-<!doctype html>
 
-<!--[if lt IE 7 ]> <html class="ie ie6 ie-lt10 ie-lt9 ie-lt8 ie-lt7 no-js" lang="en"> <![endif]-->
-<!--[if IE 7 ]>    <html class="ie ie7 ie-lt10 ie-lt9 ie-lt8 no-js" lang="en"> <![endif]-->
-<!--[if IE 8 ]>    <html class="ie ie8 ie-lt10 ie-lt9 no-js" lang="en"> <![endif]-->
-<!--[if IE 9 ]>    <html class="ie ie9 ie-lt10 no-js" lang="en"> <![endif]-->
-<!--[if gt IE 9]><!--><html class="no-js" <?php language_attributes(); ?> id="returnTop"><!--<![endif]-->
-<!-- the "no-js" class is for Modernizr. --> 
-
-<head>
-
-    <meta charset="<?php bloginfo( 'charset' ); ?>">
-    
-    <!-- Always force latest IE rendering engine (even in intranet) & Chrome Frame -->
-    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-    
-    <meta name="description" content="<?php bloginfo('description') ?>" />
-    <meta name="author" content="Findsome &amp; Winmore" />
-    <!-- Google will often use this as its description of your page/site. Make it good. -->
-    
-    <meta name="google-site-verification" content="" />
-    <!-- Speaking of Google, don't forget to set your site up: http://google.com/webmasters -->
-
-    <meta name="Copyright" content="<?php echo date('Y'); ?>" />
-
-   <meta name="viewport" content="width=device-width, initial-scale=1"/>
-   <meta name="apple-mobile-web-app-capable" content="yes" /> 
-   <meta name="apple-touch-fullscreen" content="yes" />
-
-
-   <?php get_template_part('partials/meta', 'favicons'); ?>
-
-	<?php 
-
-        /*
-         * Wordpress Head
-         */
-    
-        wp_head(); 
-
-    ?>
-</head>
-
-<body <?php body_class() ?>>
-<!--[if lt IE 8]>
-    <p class="browsehappy">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
-<![endif]-->
+<header class="main-header">
+  <div class="row">
+    <div class="columns medium-3">
+      <div class="logo">
+        
+        <img src="<?php the_field('logo', 'option') ?>">
+      </div>
+    </div>
+    <div class="columns medium-9">
+      <div class="navigation">
+        <?= render_navigation(); ?>
+      </div>
+    </div>
+  </div>
+</header>
